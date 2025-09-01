2e0b43280b11537459b8ec7be900149088231210
s.vanriessen <sander.vanriessen@bettyblocks.com>
2025-09-01 16:11:27 +0200

    issue with same interface from different package

---
+++ src/actions.js
@@ -0,0 +1,13 @@
+
+import * as $blockstore$e1ab0bfa$1 from 'betty-blocks:http-request/http';
+import * as $blockstore$15f12255$1 from 'betty-blocks:http-request-part-two/http';
+
+
+// Implementation of betty-blocks action call
+async function call(parameters) {
+  return parameters
+}
+
+export const actions = {
+  call,
+};
